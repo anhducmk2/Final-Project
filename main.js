5c9dc9cb7408767462429d30abf0be297e78247a
DinhXuanHoangg <dh347dc@gmail.com>
2023-04-17 15:30:19 +0700

    test push

---
+++ main.js
@@ -0,0 +1,28 @@
+// khởi tạo hàm
+var fullName = 'Đinh Xuân Hoàng';
+var age = 26;
+// thông báo
+// alert(fullName);
+// alert(age);
+
+// console.log('alert done')
+
+// xác nhận Y/N
+// var Confirm = 'xac minh danh tinh'
+// confirm(Confirm)
+// console.log(Confirm)
+
+// xác nhận có ô viết 
+// prompt('What your name')
+// console.log('prompt done')
+
+// thời gian chạy code deley
+// setTimeout(function(){
+//     alert('da het 1 giay')
+// }, 1000)
+
+// chạy liên tục sau thời gian xác định
+// var n = 1
+// setInterval(function(){
+//     console.log('day la code duoc chay lien tuc sau moi giay ' + n++)
+// }, 1000)
